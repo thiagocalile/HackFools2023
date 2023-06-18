c42ccfa6fbb0a996cc82f6aeacc554a61d5b3ddc
Pedro <pedrofioravanti@usp.br>
2023-06-18 13:35:34 -0300

    Código para o bot saber quais matérias têm no dia

--- dia.py
+++ dia.py
@@ -1,1 +1,13 @@
-import time
+from datetime import date
+today = date.today()
+
+dia = int(today.strftime("%w"))
+#print("d1 =", d1)
+dia =+ 5
+materia94 = [("IP, Calc"), ("TADI, SMD"), ("RP"), ("FSI, IP"), ("Calc, FSI")]
+
+if dia == 0 or dia == 6:
+    pass
+else:
+    print(materia94[dia-1])
+    
